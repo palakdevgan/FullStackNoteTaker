9683795180ee40005a68c2ef7b87c549dfe9e5ff
palakdevgan <m7.palak@gmail.com>
2022-05-02 11:16:31 -0400

    setting up the server

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,15 @@
+const router=require('express').Router();
+const { notes } = require('../../db/db.json');
+const { createNewNote } = require('../../lib/notes');
+const { v4: uuidv4 } = require('uuid');
+
+router.get('/notes',(req,res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.post('/notes',(req,res) => {
+    req.body.id=uuidv4();
+    const note=createNewNote(req.body,notes);
+    res.json(note);
+});
