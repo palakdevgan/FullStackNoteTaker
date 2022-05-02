5da5c5c468e6ae1554aa9264f07acdb6479480ef
palakdevgan <m7.palak@gmail.com>
2022-05-02 14:24:34 -0400

    copied starter code again

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const { notes } = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
