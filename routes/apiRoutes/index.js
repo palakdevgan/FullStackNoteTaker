79159ba8a1badff46d716e182691c8afe1ec20e1
palakdevgan <m7.palak@gmail.com>
2022-05-03 11:02:30 -0400

    added code for POST and DELETE

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,30 @@
 const router=require('express').Router();
-const  notes  = require('../../db/db.json');
-const { createNewNote } = require('../../lib/notes');
+const  {notes } = require('../../db/db.json');
+const { createNewNote,findById,removeById } = require('../../lib/notes');
 const { v4: uuidv4 } = require('uuid');
 
+
 router.get('/notes',(req,res) => {
-    let results = notes;
-    res.json(results);
+    res.json(notes);
 });
 
 router.post('/notes',(req,res) => {
     req.body.id=uuidv4();
     const note=createNewNote(req.body,notes);
     res.json(note);
+});
+
+router.delete('/notes/:id',(req,res) => {
+    const result = removeById(req.params.id, notes);
+    
+      if (result) 
+      {
+        res.send("DELETE Request Called")
+      } 
+      else 
+      {
+        res.send(404);
+       }
 });
 
 module.exports=router;
